f0cd31b531276c4ace883ebaa14fc71be2017115
Timilehin Aderinola <timiderinola@gmail.com>
2015-12-07 22:13:29 +0100

    Added Pages controller

---
+++ app/controllers/site_pages_controller.rb
@@ -0,0 +1,16 @@
+class SitePagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def gallery
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'site_pages/home'
+
+  get 'site_pages/about'
+
+  get 'site_pages/services'
+
+  get 'site_pages/gallery'
+
+  get 'site_pages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
